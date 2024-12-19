8020b851a2724aeb5f6768a5a99c521a5c4ad1de
Noa Aarts <noa@voorwaarts.nl>
2024-12-19 23:08:36 +0100

    add jujutsu

--- flake.nix
+++ flake.nix
@@ -165,9 +165,9 @@
 
           # Extra inputs can be added here; cargo and rustc are provided by default.
           packages = [
-            # pkgs.ripgrep
             pkgs.wgo
             pkgs.cargo-flamegraph
+            pkgs.jujutsu
           ];
         };
       }
